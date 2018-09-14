88159f29feb8569f1e0e0cde98965a62e5c37dea
standon19 <42978500+standon19@users.noreply.github.com>
2018-09-14 11:51:28 -0400

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,25 @@
+package variablehw;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	 String name = new String("Samir"); 
+
+	System.out.println(name);
+	
+	int length = 5;
+	int width = 6;
+	int area = 2*length + 2*width;
+	 System.out.println(area);
+	 
+	double pi = 3.14;
+	int radius = 3;
+	double circumference = 2*pi*radius;
+	System.out.println(circumference);
+	double circlearea = pi*radius*radius;
+	System.out.println(circlearea);
+	}
+
+}
